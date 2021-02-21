aeb9fbdac1562eaf7ee0b913ac5342558907b9cb
Mae Kindler <mae.akie.kindler@gmail.com>
2021-02-21 16:06:00 -0600

    Updating service-worker.js file with Install.

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,8 @@ const FILES_TO_CACHE = [
 	"/index.html",
 	"/index.js",
 	"/styles.css",
+	"/db.js",
+	"/manifest.webmanifest",
 	"/icons/icon-192x192.png",
 	"/icons/icon-512x512.png"
 ];
@@ -10,3 +12,16 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// Install
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// Activate
